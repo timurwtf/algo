16bb108b8b4c2ea7a386e92a8a9c0b99dc2bdc67
Timur Kanipov <139258382+timurwtf@users.noreply.github.com>
2025-03-04 18:17:26 +0300

    Add files via upload

--- snippets/template.cpp
+++ snippets/template.cpp
@@ -6,12 +6,12 @@ using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
 
-const int N = 200005;
-const int BIGN = 1000005;
-const int MOD1 = 1000000007;
-const int MOD2 = 998244353;
-const int INF = 1000000001;
-const ll INFLL = 1000000000000000001;
+const int N = 200'005;
+const int BIGN = 1'000'005;
+const int MOD1 = 1'000'000'007;
+const int MOD2 = 998'244'353;
+const int INF = 1'000'000'001;
+const ll INFLL = 1'000'000'000'000'000'001;
 const ld EPS = 1e-10;
 const ld PI = acos(-1);
 
@@ -19,6 +19,6 @@ signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  // your code here
+  
   return 0;
 }
