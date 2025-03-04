e60470382ff5d93fe6ec47dbdc2908d22853e82f
Timur Kanipov <139258382+timurwtf@users.noreply.github.com>
2025-03-04 18:18:49 +0300

    Update sparse-table.cpp

--- snippets/sparse-table.cpp
+++ snippets/sparse-table.cpp
@@ -4,7 +4,7 @@ struct SparseTable {
   vector<int> log;
 
   Type merge(Type &a, Type &b) {
-    return ; // you need to return F(a, b) here
+    return ; // you need to return f(a, b) here
   }
 
   SparseTable(vector<Type> &v) {
