60c0f37cee680bd2a05a0d8a7d3aadda7b46f25d
Timur Kanipov <139258382+timurwtf@users.noreply.github.com>
2025-03-07 17:49:37 +0300

    Add files via upload

--- snippets/tree.cpp
+++ snippets/tree.cpp
@@ -11,9 +11,8 @@ struct Tree {
 
   vector<int> depth, prev;
   vector<int> topsort, tin, tout, eulerian;
-  vector<vector<int>> up;
   vector<vector<Edge>> adj;
-  int root, mxlog, n;
+  int n;
 
   Tree() : Tree(0) {}
   Tree(int _n) : n(_n) {
@@ -22,18 +21,6 @@ struct Tree {
     tin.assign(n, -1);
     tout.assign(n, -1);
     adj.resize(n);
-  }
-
-  void clear() {
-    prev.clear();
-    depth.clear();
-    topsort.clear();
-    tin.clear();
-    tout.clear();
-    eulerian.clear();
-    up.clear();
-    adj.clear();
-    root = 0, mxlog = 0, n = 0;
   }
 
   void add_edge(int x, int y, ll c = 1) {
@@ -63,47 +50,5 @@ struct Tree {
 
   bool is_ancestor(int x, int y) {
     return tin[x] < tin[y] && tout[x] > tout[y];
-  }
-
-  void init_lca(int _root) {
-    root = _root;
-    dfs(root, root);
-    mxlog = __lg(n) + 1;
-    up.assign(mxlog, vector<int>(n));
-    for (int i = 0; i < n; ++i) {
-      up[0][i] = prev[i];
-    }
-    for (int j = 1; j < mxlog; ++j) {
-      for (int i = 0; i < n; ++i) {
-        up[j][i] = up[j - 1][up[j - 1][i]];
-      }
-    }
-  }
-
-  int la(int x, int k) {
-    k = min(k, depth[x]);
-    for (int i = mxlog - 1; i >= 0; --i) {
-      if (k & (1 << i)) {
-        x = up[i][x];
-      }
-    }
-    return x;
-  }
-
-  int lca(int x, int y) {
-    if (depth[x] > depth[y]) {
-      swap(x, y);
-    }
-    y = la(y, depth[y] - depth[x]);
-    if (x == y) {
-      return x;
-    }
-    for (int i = mxlog - 1; i >= 0; --i) {
-      if (up[i][x] != up[i][y]) {
-        x = up[i][x];
-        y = up[i][y];
-      }
-    }
-    return up[0][x];
   }
 };
